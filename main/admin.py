88a005eda5d76e5de8db6c73bd3b445beb4384b7
Rakitin Dmitry <dimrakitin@yandex.ru>
2022-05-10 19:09:09 +0300

    add new fields and fix ui

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,9 @@ from .models import Book, Catigory, Author
 
 admin.site.register(Book)
 admin.site.register(Catigory)
-admin.site.register(Author)
+# admin.site.register(Author)
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name')
+
+admin.site.register(Author, AuthorAdmin)
--- main/models.py
+++ main/models.py
@@ -1,17 +1,24 @@
 from tabnanny import verbose
+from turtle import mode
 from django.db import models
 
 class Book(models.Model):
-	title = models.CharField(max_length=200)
-	description = models.TextField()
+	title = models.CharField(max_length=200, verbose_name='Название')
+	description = models.TextField(verbose_name='Описание')
 	isbn = models.CharField(max_length=13)
 	# img = models.ImageField()
 
-	catigory = models.ManyToManyField('Catigory')
-	author = models.ManyToManyField('Author')
+	create_at = models.DateTimeField(auto_now_add=True)
+	update_at = models.DateTimeField(auto_now=True)
+
+	catigory = models.ManyToManyField('Catigory', verbose_name='Категория')
+	author = models.ManyToManyField('Author', verbose_name='Автор')
 
 	def __str__(self) :
 		return self.title
+
+	class Meta:
+		verbose_name = 'Книга'
 
 
 class Catigory(models.Model):
@@ -19,10 +26,16 @@ class Catigory(models.Model):
 
 	def __str__(self) :
 		return self.name
+	
+	class Meta:
+		verbose_name = 'Категория'
+
 
 class Author(models.Model):
 	first_name = models.CharField(max_length=100, verbose_name='Имя')
 	last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+
+	# photo = models.ImageField()
 
 	def __str__(self) :
 		return '{0} {1}'.format(self.last_name, self.first_name)
